334ab46296d7e6f5383d76779e4fdbf16e81665e
Tripp Roberts <tripp@tripp-roberts.com>
2014-10-29 16:02:07 -0400

    Adds ensure_spotify_authorized and current_spotify_user methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def ensure_spotify_authorized
+    unless cookies[:hipster]
+      redirect_to new_spotify_session_path
+    end
+  end
+
+  def current_spotify_user
+    user = User.find(cookies.signed[:hipster])
+    RSpotify::User.new(user.spotify_hash)
+  end
+
 end
