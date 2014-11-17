6ddaae10d2aab4392323519426e971e8f0fc1157
Tripp Roberts <tripp@tripp-roberts.com>
2014-11-17 11:17:30 -0500

    Includes initializer file to include snapshot_id and external_urls in Rspotify::Playlist

--- config/initializers/playlist.rb
+++ config/initializers/playlist.rb
@@ -1,6 +1,5 @@
 module RSpotify
   class Playlist < Base
-
     def initialize(options = {})
       @collaborative = options['collaborative']
       @description   = options['description']
@@ -8,7 +7,9 @@ module RSpotify
       @images        = options['images']
       @name          = options['name']
       @public        = options['public']
+      @external_urls = options['external_urls']
       @snapshot_id   = options['snapshot_id']
+      @images        = options['images']
 
       @owner = if options['owner']
                  User.new options['owner']
@@ -19,37 +20,6 @@ module RSpotify
                       end
 
       super(options)
-    end
-
-    def tracks(offset: 0)
-
-      if @tracks_cache
-        return @tracks_cache
-      end
-
-      array = []
-      total = 1
-
-      while array.length < total
-
-        url = "users/#{@owner.id}/playlists/#{@id}/tracks" \
-          "?limit=100&offset=#{array.length}"
-
-          json = if users_credentials && users_credentials[@owner.id]
-                   User.oauth_get(@owner.id, url)
-                 else
-                   RSpotify.auth_get(url)
-                 end
-
-          array = json['items'].map do |i|
-            Track.new i['track'] unless i['track'].nil?
-          end.compact
-          total = json['total']
-
-      end
-
-      @tracks_cache = tracks
-      tracks
     end
   end
 end
