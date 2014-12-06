c073ea05b399b2163671f90923d290c772c2a6f8
Tripp Roberts <tripp@tripp-roberts.com>
2014-12-06 18:27:42 -0500

    Makes adjustments in production config file to allow assets to be compiled

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module Hipster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/assets/fonts"
-    config.assets.compile = true
   end
 end
