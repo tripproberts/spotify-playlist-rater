ed475d2653d1f71cc3785c140044ee058d8a797c
Tripp Roberts <tripp@tripp-roberts.com>
2014-12-05 20:45:59 -0500

    Removes correct migration... sigh

--- db/migrate/20141117065158_remove_playlist_attributes_from_hipster_scores.rb
+++ db/migrate/20141117065158_remove_playlist_attributes_from_hipster_scores.rb
@@ -1,9 +1,9 @@
 class RemovePlaylistAttributesFromHipsterScores < ActiveRecord::Migration
   def change
+    remove_column :hipster_scores, :playlist_id, :integer
     remove_column :hipster_scores, :owner_id, :string
     remove_column :hipster_scores, :playlist_name, :string
     remove_column :hipster_scores, :owner_name, :string
     remove_column :hipster_scores, :spotify_url, :string
-    remove_column :hipster_scores, :user_id, :integer
   end
 end
