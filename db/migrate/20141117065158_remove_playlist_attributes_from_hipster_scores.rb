71e808d5a75108ddb60e20ed746d8e1b6a881a2d
Tripp Roberts <tripp@tripp-roberts.com>
2014-12-05 20:43:24 -0500

    Deletes an old migration

--- db/migrate/20141117065158_remove_playlist_attributes_from_hipster_scores.rb
+++ db/migrate/20141117065158_remove_playlist_attributes_from_hipster_scores.rb
@@ -1,6 +1,5 @@
 class RemovePlaylistAttributesFromHipsterScores < ActiveRecord::Migration
   def change
-    remove_column :hipster_scores, :playlist_id, :string
     remove_column :hipster_scores, :owner_id, :string
     remove_column :hipster_scores, :playlist_name, :string
     remove_column :hipster_scores, :owner_name, :string
