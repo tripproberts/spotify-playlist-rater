792f9ec17ea7f681e34bbff47e3ccc57736b656f
Tripp Roberts <tripp@tripp-roberts.com>
2014-12-06 18:19:39 -0500

    Set compile assets to true in application config. Trying anything to get images to show up on Heroku...

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Hipster
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/assets/fonts"
-    config.assets.initialize_on_precompile = false
+    config.assets.compile = true
   end
 end
