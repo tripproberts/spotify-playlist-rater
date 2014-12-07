ed10a6892b6be202ebd2b9fabeeae003305b0e2c
Tripp Roberts <tripp@tripp-roberts.com>
2014-12-06 22:40:34 -0500

    Adds a check in Playlist.update_attributes for users without display_names

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -9,7 +9,8 @@ class Playlist < ActiveRecord::Base
   def update_attributes
     RSpotify.authenticate(ENV["SPOTIFY_ID"], ENV["SPOTIFY_SECRET"])
     @r_playlist = RSpotify::Playlist.find(owner_id, spotify_id)
-    args = {name: @r_playlist.name, owner_name: @r_playlist.owner.display_name}
+    name = @r_playlist.owner.display_name.blank? ? @r_playlist.owner.id : @r_playlist.owner.display_name
+    args = {name: @r_playlist.name, owner_name: name}
     if @r_playlist.public
       args[:spotify_url] = @r_playlist.external_urls["spotify"]
     end
